a3a944a81876efc6a61e646d7afcc8fad4327d20
gr412_krmpe <k.maks21@mail.ru>
2023-11-14 10:25:43 +0700

    Работа с html

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -17,10 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-
-
-
-
 urlpatterns = [path('admin/', admin.site.urls), ]
 
 from django.urls import include
@@ -28,7 +24,9 @@ from django.urls import path
 
 urlpatterns += [path('catalog/', include('catalog.urls')), ]
 from django.views.generic import RedirectView
+
 urlpatterns += [path('', RedirectView.as_view(url='/catalog/', permanent=True)), ]
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
