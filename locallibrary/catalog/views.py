91ad58d11ad8fa47f07e7689fcd8504401ec9d84
MaksimKrishtopa <k.maks21@mail.ru>
2023-11-14 18:16:35 +0700

    Сессии

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -18,7 +18,7 @@ def index(request):
         'index.html',
         context={'num_books': num_books, 'num_instances': num_instances,
                  'num_instances_available': num_instances_available, 'num_authors': num_authors,
-                 'num_visits': num_visits},
+                 'num_visits': num_visits},  # num_visits appended
     )
 
 
